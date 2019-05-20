bc0c3cd7bf10735ce873a0e532729a520cbe9d11
tjrgml196 <49221338+tjrgml196@users.noreply.github.com>
2019-05-20 18:00:24 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -1,38 +1,35 @@
+/* 
+
+	사용할 함수 참고
+
+	파일 오픈하기 fopen						// [내용물 출력하기]https://blog.naver.com/vkkv456/221286419303
+	파일 구조체를 이용하여 문자열을 가져오기 char *fgets(char *str, int numChars, FILE *stream); // [fgets사용법]https://blog.naver.com/zzzz4022/221439119422
+
+
+*/
+
+#define MAX_LENGTH 100
 #include <stdio.h>
 
-void sum(int num1, int num2) {
-	printf("Sum result : %d\n", num1 + num2);
-}
-
-void sub(int num1, int num2) {
-	printf("Sub result : %d\n", num1 - num2);
-}
-
-void mul(int num1, int num2) {
-	printf("mul result : %d\n", num1 * num2);
-}
-
-void di(int num1, int num2) {
-	printf("div result : %d\n", num1 / num2);
-}
-
-void rem(int num1, int num2) {
-	printf("remainder : %d\n", num1 % num2);
-}
 
 int main(void) {
-	int n1, n2;
+	
+	int line_count = 0;							// 줄 수 세는 카운트 변수
+	char buffer[MAX_LENGTH];					//
+	FILE* p_file = NULL;						//
 
-	printf("Enter first number :");
-	scanf_s("%d", &n1);		// input first number
+	if (fopen_s(&p_file, "mips.txt", "r") == 0) {		// fopen_s 는 파일이 열리면 0을 반환
 
-	printf("Enter second number :");
-	scanf_s("%d", &n2);		// input second number
+		while (fgets(buffer, MAX_LENGTH, p_file) != NULL) {
+			line_count++;
+			printf("[%d] %s", line_count, buffer);
+		}
+		fclose(p_file);
 
-	sum(n1, n2);	// +
-	sub(n1, n2);	// -
-	mul(n1, n2);    // *
-	di(n1, n2);		// /
-	rem(n1, n2);	// %
+	}
+	else
+		printf("파일이 잘못되었습니다.\n");
 
+
+	return 0;
 }
